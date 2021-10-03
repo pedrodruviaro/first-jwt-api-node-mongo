5f372544d64c0fc07c056103d74cf5f12dfb389e
pedrodruviaro <pedrodruviaro@gmail.com>
2021-10-03 18:09:38 -0300

    iniciando rota de registro

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const authRoute = require('./routes/auth')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
+
+// connecting to mongoDB
+mongoose.connect(process.env.MONGO_URI, () => console.log("connected to mongodb"))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/api/user', authRoute)
+
 
 app.listen(3000, () => console.log('Server running!'))
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,22 @@
 const router = require('express').Router()
+const User = require('../model/User')
 
-router.post('/register', (req, res) => {
-    res.send("Register")
+router.post('/register', async (req, res) => {
+    
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    })
+
+    try {
+        
+        const savedUser = await user.save()
+        res.status(200).json(savedUser)
+
+    } catch (error) {
+        res.status(400).json(error)
+    }
 })
 
 module.exports = router
