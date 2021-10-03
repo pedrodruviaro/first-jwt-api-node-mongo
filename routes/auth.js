339e63bbb07e33e483481435d5802a997a8b916e
pedrodruviaro <pedrodruviaro@gmail.com>
2021-10-03 18:52:17 -0300

    validando com @hapi/joi

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,35 @@
 const router = require('express').Router()
 const User = require('../model/User')
 
+// VALIDATION
+const Joi = require('@hapi/joi')
+const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required()
+})
+
 router.post('/register', async (req, res) => {
+
+    // lets validate the data before we make the user
+    const validation = schema.validate(req.body)
     
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    })
+    res.send(validation)
 
-    try {
+    // const user = new User({
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: req.body.password,
+    // })
+
+    // try {
         
-        const savedUser = await user.save()
-        res.status(200).json(savedUser)
+    //     const savedUser = await user.save()
+    //     res.status(200).json(savedUser)
 
-    } catch (error) {
-        res.status(400).json(error)
-    }
+    // } catch (error) {
+    //     res.status(400).json(error)
+    // }
 })
 
 module.exports = router
